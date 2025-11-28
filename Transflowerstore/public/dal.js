162263b822c66d6badbee81a934ab1cdc39cc31f
rutujamokale <rutujamokale2003@gmail.com>
2025-11-28 15:33:17 +0530

    database connectivity using js

--- Transflowerstore/public/dal.js
+++ Transflowerstore/public/dal.js
@@ -4,9 +4,23 @@ var dbServer={
     user:'root',
     password:'password',
 
-    database:assessmentdb
+    database:'assessmentdb'
 }
 var connection=mysql.createConnection();
 connection.connect(function(err){
     console.log(err);
 });
+
+    var selectQuery="select * from roles";
+
+
+    connection.query(selectQuery,function(err, data){
+        if(err){
+            console.log("error: "+err);
+        }
+        else{
+            console.log(data);
+        }
+    });
+    console.log("such a simple way to access data from tables");
+    
